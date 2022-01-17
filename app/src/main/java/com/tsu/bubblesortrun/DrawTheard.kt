261e9d36581ac7f579c71933e9fa8921aec29b8a
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-01-17 23:53:43 +0700

    add background winter Maiden, Father Frost and university on SurfaceView

--- app/src/main/java/com/tsu/bubblesortrun/DrawTheard.kt
+++ app/src/main/java/com/tsu/bubblesortrun/DrawTheard.kt
@@ -1,6 +1,5 @@
 package com.tsu.bubblesortrun
 
-import android.R
 import android.content.res.Resources
 import android.graphics.*
 import android.view.SurfaceHolder
@@ -8,10 +7,10 @@ import android.view.SurfaceHolder
 internal class DrawThread(private val surfaceHolder: SurfaceHolder, resources: Resources?) :
     Thread() {
     private var runFlag = false
-    private val picture: Bitmap
+    private val hodhedhog: Bitmap
     private val fatherFrost: Bitmap
     private val winterMaiden: Bitmap
-
+    private val university: Bitmap
     fun setRunning(run: Boolean) {
         runFlag = run
     }
@@ -24,15 +23,14 @@ internal class DrawThread(private val surfaceHolder: SurfaceHolder, resources: R
                 // получаем объект Canvas и выполняем отрисовку
                 canvas = surfaceHolder.lockCanvas(null)
                 synchronized(surfaceHolder) {
-                    canvas.drawBitmap(picture, 0f,0f, null)
+                    canvas.drawBitmap(hodhedhog, 0f,0f, null)
                     canvas.drawBitmap(fatherFrost, 250f,250f, null)
                     canvas.drawBitmap(winterMaiden, 350f,350f, null)
-                }
-            } finally {
-                if (canvas != null) {
-                    // отрисовка выполнена. выводим результат на экран
+                    canvas.drawBitmap(university, 200f,350f, null)
                     surfaceHolder.unlockCanvasAndPost(canvas)
                 }
+            } finally {
+
             }
         }
     }
@@ -40,17 +38,17 @@ internal class DrawThread(private val surfaceHolder: SurfaceHolder, resources: R
     init {
 
         // загружаем картинку, которую будем отрисовывать
-        picture = BitmapFactory.decodeResource(
-            resources,
-            com.tsu.bubblesortrun.R.drawable.hodhedhog
+        hodhedhog = BitmapFactory.decodeResource(
+            resources, R.drawable.hodhedhog
         )
         fatherFrost = BitmapFactory.decodeResource(
-            resources,
-            com.tsu.bubblesortrun.R.drawable.father_frost
+            resources, R.drawable.father_frost
         )
         winterMaiden= BitmapFactory.decodeResource(
-            resources,
-            com.tsu.bubblesortrun.R.drawable.winter_maiden
+            resources,R.drawable.winter_maiden
+        )
+        university= BitmapFactory.decodeResource(
+            resources,R.drawable.university
         )
     }
 }
