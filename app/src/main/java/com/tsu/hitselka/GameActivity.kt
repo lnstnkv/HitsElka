edd81152ea47a7aedf9972b8cd4866afd023d505
Vladislav Netaev <vladnetaev@mail.ru>
2022-01-18 18:47:16 +0700

    Added controls and background

--- app/src/main/java/com/tsu/hitselka/GameActivity.kt
+++ app/src/main/java/com/tsu/hitselka/GameActivity.kt
@@ -1,11 +1,27 @@
 package com.tsu.hitselka
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.tsu.hitselka.databinding.ControlsBinding
+import android.view.ViewGroup
+import com.tsu.hitselka.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityGameBinding.inflate(layoutInflater) }
+    private val controls by lazy { ControlsBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(SurfaceView(this))
+
+        val surface = SurfaceView(this)
+        setContentView(surface)
+
+        val lp = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        val background = LayoutInflater.from(this).inflate(R.layout.activity_game, null, false)
+        addContentView(background, lp)
+
+        val controlsOverlay = LayoutInflater.from(this).inflate(R.layout.controls, null, false)
+        addContentView(controlsOverlay, lp)
     }
 }
