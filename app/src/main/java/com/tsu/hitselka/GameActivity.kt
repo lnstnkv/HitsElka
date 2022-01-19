ba237c2de88c2190c1626f65daa6f2824bf1d891
Vladislav Netaev <vladnetaev@mail.ru>
2022-01-19 17:14:32 +0700

    Added sounds and background music

--- app/src/main/java/com/tsu/hitselka/GameActivity.kt
+++ app/src/main/java/com/tsu/hitselka/GameActivity.kt
@@ -1,12 +1,11 @@
 package com.tsu.hitselka
 
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.util.Log
-import android.view.*
-import androidx.appcompat.app.AppCompatActivity
-import com.tsu.hitselka.databinding.ControlsBinding
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -15,12 +14,14 @@ import com.bumptech.glide.request.target.Target
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.tsu.hitselka.databinding.ActivityGameBinding
-import com.tsu.hitselka.model.GameLogic
+import com.tsu.hitselka.databinding.ControlsBinding
 
 class GameActivity : AppCompatActivity(R.layout.controls) {
     private val binding by lazy { ActivityGameBinding.inflate(layoutInflater) }
     private val controls by lazy { ControlsBinding.inflate(layoutInflater) }
     private val viewModel by viewModels<GameViewModel>()
+    private lateinit var clickSound: MediaPlayer
+    private lateinit var backgroundMusic: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +29,10 @@ class GameActivity : AppCompatActivity(R.layout.controls) {
         val surface = SurfaceView(this)
         setContentView(surface)
 
-        val lp = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        val lp = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
         addContentView(binding.root, lp)
         addContentView(controls.root, lp)
 
@@ -36,35 +40,43 @@ class GameActivity : AppCompatActivity(R.layout.controls) {
         initObservers()
         setProfileImage()
         hideSystemBars()
+        setMusic()
     }
 
     private fun initListeners() {
         controls.yearSelectorImageView.setOnClickListener {
             Toast.makeText(this, "Year selector", Toast.LENGTH_SHORT).show()
+            playClickSound()
         }
 
         controls.achievementsImageView.setOnClickListener {
             Toast.makeText(this, "Achievements", Toast.LENGTH_SHORT).show()
+            playClickSound()
         }
 
         controls.giftImageView.setOnClickListener {
             Toast.makeText(this, "Gifts", Toast.LENGTH_SHORT).show()
+            playClickSound()
         }
 
         controls.wandView.setOnClickListener {
             Toast.makeText(this, "Enhance", Toast.LENGTH_SHORT).show()
+            playClickSound()
         }
 
         controls.settingsImageView.setOnClickListener {
             Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+            playClickSound()
         }
 
         controls.shopImageView.setOnClickListener {
             Toast.makeText(this, "Shop", Toast.LENGTH_SHORT).show()
+            playClickSound()
         }
 
         controls.playImageView.setOnClickListener {
             Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
+            playClickSound()
         }
     }
 
@@ -93,10 +105,38 @@ class GameActivity : AppCompatActivity(R.layout.controls) {
     private fun hideSystemBars() {
         val windowInsetsController =
             ViewCompat.getWindowInsetsController(window.decorView) ?: return
-        // Configure the behavior of the hidden system bars
         windowInsetsController.systemBarsBehavior =
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        // Hide both the status bar and the navigation bar
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+    private fun setMusic() {
+        clickSound = MediaPlayer.create(this, R.raw.click)
+
+        backgroundMusic = MediaPlayer.create(this, R.raw.music)
+        backgroundMusic.isLooping = true
+        backgroundMusic.setVolume(50f, 50f)
+        //backgroundMusic.start()
+    }
+
+    private fun playClickSound() {
+        clickSound.start()
+    }
+
+    override fun onRestart() {
+        backgroundMusic.start()
+        super.onRestart()
+    }
+
+    override fun onStop() {
+        backgroundMusic.pause()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        backgroundMusic.stop()
+        backgroundMusic.release()
+        clickSound.release()
+        super.onDestroy()
     }
 }
