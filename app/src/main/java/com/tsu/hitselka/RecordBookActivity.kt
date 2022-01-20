6424de5bb984b533de9c7df4fc5fb2553ceaece3
Elena Sitnikova <lnstnkv2002@gmail.com>
2022-01-20 10:18:17 +0700

    start make record book

---
+++ app/src/main/java/com/tsu/hitselka/RecordBookActivity.kt
@@ -0,0 +1,11 @@
+package com.tsu.hitselka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record_book)
+    }
+}
